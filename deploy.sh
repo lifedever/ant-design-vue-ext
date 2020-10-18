e4ac6b1289e644e80f4ff4906bdf88316e3921f2
gefangshuai <gefangshuai@163.com>
2020-10-18 20:57:03 +0800

    coding

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ yarn version
 git add . && git commit -m 'coding'
 git checkout master && git merge develop && git checkout develop
 git push --all && git push --tag
-npm publish
+npm publish --registry http://npm.kanasinfo.cn
