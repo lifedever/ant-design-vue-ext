8d0f14b345efa81ca6aac83f3e19ee7e88cb6978
gefangshuai <gefangshuai@163.com>
2020-10-24 13:16:59 +0800

    coding

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,6 +3,7 @@ import SplitContainer from "./container/SplitContainer";
 import TabContainer from "./container/TabContainer";
 import TableContainer from "./container/TableContainer";
 import TreeContainer from "./container/TreeContainer";
+import MenuContainer from "./container/MenuContainer";
 
 import STable from "./partial/table/STable";
 import STableColumn from "./partial/table/STableColumn";
@@ -19,6 +20,7 @@ export default {
         Vue.component('TabContainer', TabContainer)
         Vue.component('TableContainer', TableContainer)
         Vue.component('TreeContainer', TreeContainer)
+        Vue.component('MenuContainer', MenuContainer)
 
         Vue.component('STable', STable)
         Vue.component('STableColumn', STableColumn)
