2d424c742e5c46d1a58a562ae025a68e428d5e76
gefangshuai <gefangshuai@163.com>
2020-10-24 13:28:24 +0800

    coding

--- src/lib/container/MenuContainer.vue
+++ src/lib/container/MenuContainer.vue
@@ -6,23 +6,28 @@
             <slot name="headerLeft"></slot>
         </template>
         <div slot="sider">
-            <a-menu mode="inline" v-bind="menuConfig">
+            <a-menu mode="inline"
+                    @openChange="openChangeHandle"
+                    @select="selectHandle"
+                    v-bind="menuConfig">
                 <template v-for="menu in menus">
-                    <a-sub-menu v-if="menu.children" :key="menu.key">
+                    <a-sub-menu v-if="menu.children"
+                                :key="menu.key">
                         <span slot="title">
                             {{ menu.name || menu.title }}
                         </span>
                         <a-menu-item :key="subMenu.key"
+                                     @click="handlerClick(menu)"
                                      v-for="subMenu in menu.children">
                             {{ subMenu.name || subMenu.title}}
                         </a-menu-item>
                     </a-sub-menu>
                     <a-menu-item v-else
+                                 @click="handlerClick(menu)"
                                  :key="menu.key">
                         {{ menu.name || menu.title }}
                     </a-menu-item>
                 </template>
-
             </a-menu>
         </div>
         <slot></slot>
@@ -37,7 +42,18 @@
         props: Object.assign({}, BaseProps, {
             menus: Array,
             menuConfig: Object
-        })
+        }),
+        methods: {
+            handlerClick(o) {
+                this.$emit('click', o)
+            },
+            openChangeHandle(o) {
+                this.$emit('openChange', o)
+            },
+            selectHandle(o) {
+                this.$emit('select', o)
+            }
+        }
     }
 </script>
 
