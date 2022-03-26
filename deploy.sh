ea8e3fd186940ae3e6c894ae8727816e2993517a
gefangshuai <gefangshuai@163.com>
2022-03-26 11:36:12 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+
+git add . && git commit -m "new version"
 yarn version
-git add . && git commit -m 'coding'
+
+git add . && git commit -m "new version"
 git checkout master && git merge develop && git checkout develop
+
 git push --all && git push --tag
-npm publish --registry http://npm.kanasinfo.cn
